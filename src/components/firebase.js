867a2793e051e5a82bfd1756540ae63840c933c0
Shivansh <shivanshgoel21@gmail.com>
2020-11-27 20:32:04 +0530

    .

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase";
+import firebase_cred from "./firebase_cred.json"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDVoktrnJaXbaEPColKuHmOV4utzyfm4So",
-    authDomain: "slack-clone-42e95.firebaseapp.com",
-    databaseURL: "https://slack-clone-42e95.firebaseio.com",
-    projectId: "slack-clone-42e95",
-    storageBucket: "slack-clone-42e95.appspot.com",
-    messagingSenderId: "205281536003",
-    appId: "1:205281536003:web:8a5629686c1e74c99c0636",
-    measurementId: "G-3N8ZBRC9C4"
-};
+const firebaseConfig = JSON.parse(firebase_cred);
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
