d678500e5fecb6cf701e932d86d11e327b7788a0
Shivansh Goel <shivanshgoel21@gmail.com>
2021-02-23 23:11:08 +0530

    update contact information on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -23,9 +23,24 @@ function Login() {
 
     return (
         <div className="login">
-            <div className="login__container">
+            <div className="login__container" style={{padding:"80px"}}>
                 <img src="https://cdn.jim-nielsen.com/ios/512/spike-email-messenger-chat-2019-05-01.png" alt="logo" />
-                <h1>Hey.. Sign In to Drop a Message in My Clone</h1>
+                <h1>Hey 👋 Nice to see you here.</h1>
+                <div style={{margin:"10px"}}>
+                    <a className="ui linkedin button" href="https://www.linkedin.com/in/shivansh-goel-260019a7/" target="_blank">
+                        <i className="linkedin icon"></i>
+                        LinkedIn
+                        </a>
+                    <a className="ui instagram button" href="https://www.instagram.com/_shivansh_21/" target="_blank">
+                        <i className="instagram icon"></i>
+                        Instagram
+                    </a>
+                    <a className="ui github button" href="https://github.com/ishivanshgoel" target="_blank">
+                        <i className="github icon"></i>
+                        Github
+                    </a>
+                </div>
+                <h1>Sign In to drop a Message. 👨‍💻</h1>
                 <button onClick={signIn} className="ui google plus button">
                     <i className="google plus icon"></i>Sign In with Google</button>
             </div>
